bc82d67aeca3cd95559cbcc49d5bcc69643aca4c
Jesse Wellenberg <48557715+Yeseh@users.noreply.github.com>
2022-07-08 20:10:28 +0200

    Initial dotnet template example

--- templates/dotnet-ms/run.sh
+++ templates/dotnet-ms/run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 gh auth login --with-token
-gh repo create Yeseh/SkafTest --public
-gh repo clone Yeseh/SkafTest
+mkdir SkafTest
 cd SkafTest/
 
 # Init Dotnet project
+git init
 dotnet new sln
 dotnet new webapi -o SkafTest.API -minimal --no-restore
 dotnet new xunit -o SkafTest.Tests --no-restore
 dotnet sln add SkafTest.API SkafTest.Tests
 
-git commit -am "Initial commit with Skaf"
+git add -A
+git commit -m "Initial commit with Skaf"
 git branch -M main
+
+gh repo create Yeseh/SkafTest --public
+git remote add origin "https://yeseh:$GH_TOKEN@github.com/Yeseh/SkafTest"
+
 git push -u origin main
